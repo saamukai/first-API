4b0da38cc1a35958b27739e5e004a7427f378059
saamukai <contato.samukajpc@gmail.com>
2023-02-14 11:55:37 -0300

    primeira api finalzada

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {'mensagem': 'Testando Rotas - Home'}
+class Usuario(BaseModel):
+    id: int
+    nome: str
+    senha: str
+    endereco: Optional[str]
 
+users = [
+    Usuario(id=1, nome='usuario um', senha='senhadouser1'),
+    Usuario(id=2, nome='usuario dois', senha='senha douser2'),
+    Usuario(id=3, nome='usuario tres', senha='senhadouser3')
+]
 
-@app.get("/cadastro")
-def cadastro():
-    return {'mensagem': 'Testando Rotas - Cadastro'}
+@app.post('/cadastrouser')
+def cadastrarUser(usuario: Usuario):
+    users.append(usuario)
+    return 'Usuario Cadastrado'
 
+@app.get('/users')
+def listUser():
+    return users
 
-@app.get("/login")
-def login():
-    return {'mensagem': 'Testando Rotas - Login'}
+@app.get('/user/{id}')
+def searchById(id: int):
+    for i in users:
+        if i.id == id:
+            return i
+
+    return "User not exists"
--- test-reqs.py
+++ test-reqs.py
@@ -11,5 +11,5 @@ url = 'http://127.0.0.1:8000'
 # retorno = requests.post("http://127.0.0.1:8000/user", params={"nome": 'brasilia amarela'})
 # print(retorno.json())
 
-retorno = requests.post(f'{url}/cadastrouser', params={'id': 4, 'nome': 'usuario quatro', 'senha': 'senhauser4'})
-print(retorno)
+# retorno = requests.post(f'{url}/cadastrouser', params={'id': 4, 'nome': 'usuario quatro', 'senha': 'senhauser4'})
+# print(retorno)
